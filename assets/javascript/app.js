912031b13d043d1f5c962b3236f1f73c77974be1
jon alden <jondevalden@gmail.com>
2019-08-07 15:08:06 -0500

    minor changes

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+
+    // create a start-button that is alone on the page
+    // when start-button is pressed, hide button and display trivia questions
+    // create a timer that starts when the user hits the start-game button
+    // add questions to the game that need to be answered
+    // add 4 possible answers, only one of which is correct. user can only select one option per question
+    // give the user a way to select the answer they think is correct
+    // return the users guess to the correct win/loss counter
+    // When timer runs out hide all questions and display user stats
+    // create a finsh-game button that hides all questions and displays user stats
+
+// global variables
+
+let correctAnswers = 0;
+let wrongAnswers = 0;
+let unanswered = 0;
